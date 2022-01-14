d9cb320303b67c221a057d8b3b073ef49ea75c93
kitmi <juadolfob@gmail.com>
2022-01-14 13:46:57 -0700

    Update Springboot2MssqlJpaHibernateCrudExampleApplicationTests.java

--- src/test/java/net/guides/springboot2/crud/Springboot2MssqlJpaHibernateCrudExampleApplicationTests.java
+++ src/test/java/net/guides/springboot2/crud/Springboot2MssqlJpaHibernateCrudExampleApplicationTests.java
@@ -7,6 +7,8 @@ import static org.hamcrest.Matchers.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith; 
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -27,6 +29,7 @@ import net.guides.springboot2.crud.model.Employee;
 import net.guides.springboot2.crud.repository.EmployeeRepository;
 
 @ExtendWith(MockitoExtension.class)
+@TestInstance(Lifecycle.PER_CLASS)
 class Springboot2MssqlJpaHibernateCrudExampleApplicationTests {
 
 	private MockMvc mockMvc;
@@ -41,11 +44,11 @@ class Springboot2MssqlJpaHibernateCrudExampleApplicationTests {
 	private EmployeeRepository employeeRepository;
 	
 	
-	Employee employee_1 = new Employee(1L,"f","l","i");
-	Employee employee_2 = new Employee(2L,"f","l","i");
-	Employee employee_3 = new Employee(3L,"f","l","i");
+	Employee employee_1 = new Employee(1L,"Sara","l","i");
+	Employee employee_2 = new Employee(2L,"Juan","l","i");
+	Employee employee_3 = new Employee(3L,"Peter","l","i");
 	
-	@BeforeEach
+	@BeforeAll
 	void setUp() {
 		MockitoAnnotations.openMocks(this);
 		this.mockMvc = MockMvcBuilders.standaloneSetup(employeeController).build();
@@ -58,7 +61,24 @@ class Springboot2MssqlJpaHibernateCrudExampleApplicationTests {
 		mockMvc.perform(MockMvcRequestBuilders
 				.get("/api/v1/employees")
 				.contentType(MediaType.APPLICATION_JSON))
-				.andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(3)));
+				.andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(3)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].firstName", is("Sara")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[1].firstName", is("Juan")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[2].firstName", is("Peter")));
+	}
+	
+	//TODO https://youtu.be/KYkEMuA50yE?t=4601
+	@Test
+	public void getEmployeeById() throws Exception {
+		List<Employee> record = new ArrayList<>(Arrays.asList(employee_1,employee_2,employee_3));
+		Mockito.when(employeeRepository.findAll()).thenReturn(record);
+		mockMvc.perform(MockMvcRequestBuilders
+				.get("/api/v1/employees")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(3)))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].firstName", is("Sara")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[1].firstName", is("Juan")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[2].firstName", is("Peter")));
 	}
 
 }
